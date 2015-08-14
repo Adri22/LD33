16539cf05f3660f8d55f41516794c579b0730e7a
Adrian Böhm <adrian.boehm22@googlemail.com>
2015-08-15 00:46:42 +0200

    ld33 libgdx init

---
+++ desktop/src/de/yellowapple/ld33/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.yellowapple.ld33.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.yellowapple.ld33.LD33Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD33Main(), config);
+	}
+}
